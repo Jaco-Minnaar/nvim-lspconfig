7fb3e5530093e77709c364e98a2590e085f2dce6
Jaco Minnaar <jminnaar@bscglobal.com>
2023-12-08 12:16:26 +0200

    fix: split command

--- lua/lspconfig/server_configurations/csharpierls.lua
+++ lua/lspconfig/server_configurations/csharpierls.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    cmd = { 'csharpier-ls', '-m stdio' },
+    cmd = { 'csharpier-ls', '-m', 'stdio' },
     filetypes = { 'cs' },
     root_dir = function(fname)
       return util.root_pattern('*.sln', '.git')(fname)
